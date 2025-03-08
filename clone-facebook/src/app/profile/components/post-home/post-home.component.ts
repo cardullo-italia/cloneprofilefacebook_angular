70b12a610776322524a6f9278138954ab727ed1f
cardullo-italia <109167867+cardullo-italia@users.noreply.github.com>
2025-03-08 16:54:14 +0100

    feat: navbar
    + Incompleted post-home
    
    Done nav bar

---
+++ clone-facebook/src/app/profile/components/post-home/post-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BreveComponent } from "../breve/breve.component";
+
+@Component({
+  selector: 'app-post-home',
+  imports: [BreveComponent],
+  templateUrl: './post-home.component.html',
+  styleUrl: './post-home.component.css'
+})
+export class PostHomeComponent {
+
+}
--- clone-facebook/src/app/profile/profile-home/profile-home.component.ts
+++ clone-facebook/src/app/profile/profile-home/profile-home.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ImageCoverComponent } from "../components/image-cover/image-cover.component";
 import { PartSinistraComponent } from "../components/part-sinistra/part-sinistra.component";
 import { PartDestraComponent } from "../components/part-destra/part-destra.component";
+import { NavbarComponent } from "../components/navbar/navbar.component";
+import { PostHomeComponent } from "../components/post-home/post-home.component";
 
 @Component({
   selector: 'app-profile-home',
-  imports: [ImageCoverComponent, PartSinistraComponent, PartDestraComponent],
+  imports: [ImageCoverComponent, PartSinistraComponent, PartDestraComponent, NavbarComponent, PostHomeComponent],
   templateUrl: './profile-home.component.html',
   styleUrl: './profile-home.component.css'
 })
